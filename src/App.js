c97059d7012069156167d7aaffdb0e6b22e2ee5a
Doruk Topcu <doruktopcu@heimatlas.com>
2022-09-30 00:29:52 +0300

    New component added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItems'
 import './App.css';
 
 function App() {
   return (
     <body>
       Let's goo!
+      <ExpenseItem></ExpenseItem>
+
     </body>
   );
 }
